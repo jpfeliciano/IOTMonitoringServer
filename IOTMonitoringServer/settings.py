327a2488b905dd1ddab856a7e5f6993df654d845
Juan Feliciano <jp.feliciano10@uniandes.edu.co>
2022-07-24 11:51:28 -0500

    Actualización de IP's por re-inicio de laboratorio.

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "184.72.125.208"]
+ALLOWED_HOSTS = ["localhost", "75.101.251.84"]
 
 
 # Application definition
@@ -95,7 +95,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "54.208.104.13",  # Dirección IP de la base de datos
+        "HOST": "18.234.57.0",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
@@ -155,7 +155,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "54.145.187.125"
+MQTT_HOST = "3.94.93.168"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082

0efe69a5281373ea5cb3d12b8b3aade93011e51f
Juan Feliciano <jp.feliciano10@uniandes.edu.co>
2022-07-24 20:41:16 -0500

    Actualización de IP de base de datos por reinicio de instancia.

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "54.224.156.203",  # Dirección IP de la base de datos
+        "HOST": "3.90.253.44",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
